0b61387a9e59962d95034dc013908f9946854b45
wlo-dev <chagulukawillouby0@gmail.com>
2026-06-25 13:32:39 +0200

    more commits

--- template.py
+++ template.py
@@ -1,3 +1,15 @@
 import os
 from pathlib import Path
 import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+list_of_files = [
+    "src/init_.py"
+    "src/helper.py"
+    ".env",
+    "requirements.txt",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb"
+]
