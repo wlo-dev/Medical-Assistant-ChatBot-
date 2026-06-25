279d6f8fab58a88b5246a3c1759c529eaad2c886
wlo-dev <chagulukawillouby0@gmail.com>
2026-06-25 14:40:52 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI project',
+    version = '0.0.1',
+    author ='Willouby Chaguluka',
+    author_email ='chagulukawillouby1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+    
+)
--- template.py
+++ template.py
@@ -5,11 +5,30 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
 
 list_of_files = [
-    "src/init_.py"
-    "src/helper.py"
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
     ".env",
-    "requirements.txt",
     "setup.py",
     "app.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "test.py"
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    print(f"Processing: {filepath} | filedir='{filedir}' | filename='{filename}'")
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filename}")
+
+    else:
+        logging.info(f"{filename} already exists")
