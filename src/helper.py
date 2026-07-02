39835f5329806f2a8a06ae50f23d97f407b62b13
wlo-dev <chagulukawillouby0@gmail.com>
2026-07-02 16:13:45 +0200

    Added functionla code to helper.py

---
+++ src/helper.py
@@ -0,0 +1,3 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
