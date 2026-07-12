e83908b2d084efedecc7243e3709ba3c5be7f2f5
wlo-dev <chagulukawillouby0@gmail.com>
2026-07-12 02:20:18 +0200

    helper.py additions

--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,12 @@
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
+
+
+#Extract Data from the PDF file
+def load_pdf_file(data):
+    loader = DirectoryLoader(data
+                             glob="*.pdf",
+                             loader_cls=PyPDFLoader)
+    
+    documents =loader.load()
