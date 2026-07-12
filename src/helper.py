22736a4cd142842e84a6186e86f37d73c7b7a1c3
wlo-dev <chagulukawillouby0@gmail.com>
2026-07-12 02:27:43 +0200

    chunking function

--- src/helper.py
+++ src/helper.py
@@ -5,8 +5,17 @@ from langchain.embeddings import HuggingFaceEmbeddings
 
 #Extract Data from the PDF file
 def load_pdf_file(data):
-    loader = DirectoryLoader(data
+    loader = DirectoryLoader(data,
                              glob="*.pdf",
                              loader_cls=PyPDFLoader)
     
     documents =loader.load()
+    
+    return documents
+
+#Slite the data into chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    texts = text_splitter.split-documents(extracted_data)
+    return text_chunks
+    
