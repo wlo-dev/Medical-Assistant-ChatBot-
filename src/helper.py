b2d4bc980d60da70e2783b3805a6b6f72c2b8b5e
wlo-dev <chagulukawillouby0@gmail.com>
2026-07-12 02:33:09 +0200

    embedding function

--- src/helper.py
+++ src/helper.py
@@ -13,9 +13,14 @@ def load_pdf_file(data):
     
     return documents
 
-#Slite the data into chunks
+#Split the data into chunks
 def text_split(extracted_data):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-    texts = text_splitter.split-documents(extracted_data)
+    text_chunks = text_splitter.split_documents(extracted_data)
     return text_chunks
+
+#Download the embeddings from HuggingFace
+def download_hugging_face_embeddings():
+    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    return embeddings
     
