9bf3ac92df37fb41a6df15a8f4104cf84cb2ab94
wlo-dev <chagulukawillouby0@gmail.com>
2026-07-12 03:03:06 +0200

    index creating

--- src/store_index.py
+++ src/store_index.py
@@ -10,3 +10,23 @@ load_dotenv()
 
 PINECONE_API_KEY=os.getenv('PINECONE_API_KEY')
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
+
+extracted_data=load_pdf_file(data='data/')
+text_chunks=text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+index_name = "medicalbot"
+
+if index_name not in [i["name"] for i in pc.list_indexes()]:
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+else:
+    print(f"Index '{index_name}' already exists — skipping creation.")
+
+index = pc.Index(index_name)
